ef8ea743639d97af4353c01b19b3c322c1e0b362
simon-schaefer <simon.k.schaefer@gmail.com>
2021-06-25 10:24:23 +0200

    bug: visualization max-plots not forced to be square anymore

--- aegnn/visualize/batch.py
+++ aegnn/visualize/batch.py
@@ -9,7 +9,7 @@ from .data import event_histogram
 
 
 def bbox(batch: torch_geometric.data.Batch, predictions: torch.Tensor = None, titles: List[str] = None,
-         max_plots: int = 36, fig_size: int = 2):
+         max_plots: int = 36, fig_size: int = 2, max_plots_row: int = 4):
     """Plot the event graphs stored in the batch (or some of them) as histograms and draw the ground-truth
     detection bounding box(es) above them. If available, the predicted bounding boxes are drawn as well.
 
@@ -18,20 +18,28 @@ def bbox(batch: torch_geometric.data.Batch, predictions: torch.Tensor = None, ti
     :param titles: image titles, default = None, i.e. image class is used.
     :param max_plots: maximal number of plots (default = 36), should have an integer square root.
     :param fig_size: figure size = number of plots per axis * fig_size (default = 2).
+    :param max_plots_row: maximal number of plots per row (default = 4).
     """
     num_graphs = min(batch.num_graphs, max_plots)
     ax_size = int(np.sqrt(num_graphs))
     if titles is not None:
         assert num_graphs <= len(titles)
-    assert num_graphs == ax_size * ax_size, "number of plots should be evenly square-able"
 
     # Get ground-truth bounding boxes and sample random indices to plot.
     bb_gt = getattr(batch, "bbox")
     bb_gt_index = getattr(batch, "batch_bbox")
     indices = random.sample(list(np.arange(0, batch.num_graphs)), k=num_graphs)
 
+    # Adapt the plot distribution to the number of graphs.
+    if num_graphs < max_plots_row:
+        ax_size_x = 1
+        ax_size_y = num_graphs
+    else:
+        ax_size_x = max_plots_row
+        ax_size_y = num_graphs // max_plots_row
+
     # Plot every single graph individually based on functions defined in `data`.
-    fig, ax = plt.subplots(ax_size, ax_size, figsize=(ax_size * fig_size, ax_size * fig_size))
+    fig, ax = plt.subplots(ax_size_x, ax_size_y, figsize=(ax_size_y * fig_size, ax_size_x * fig_size))
     for iax, i in enumerate(indices):
         bbox_i = None
         if predictions is not None:
@@ -41,8 +49,18 @@ def bbox(batch: torch_geometric.data.Batch, predictions: torch.Tensor = None, ti
         sample = batch.to_data_list()[i]
         sample.bbox = bb_gt[bb_gt_index == i].view(-1, 5)
 
-        xax = iax // ax_size
-        yax = iax % ax_size
-        axis = ax[xax, yax] if batch.num_graphs > 1 else ax
+        axis = __get_axis(ax, iax, num_graphs=num_graphs, max_plots_row=max_plots_row)
         event_histogram(sample, bbox=bbox_i, title=title_i, ax=axis)
     plt.show()
+
+
+def __get_axis(ax: plt.Axes, iax: int, num_graphs: int, max_plots_row: int):
+    xax = iax // max_plots_row
+    yax = iax % max_plots_row
+    if num_graphs == 1:
+        axis = ax
+    elif num_graphs <= max_plots_row:
+        axis = ax[yax]
+    else:
+        axis = ax[xax, yax]
+    return axis
